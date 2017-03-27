61f9116e2f3bcac17fffc8200df662ec083f36bd
dsweetser <daniel.sweetser@gmail.com>
2017-03-26 23:29:08 -0400

    Test curl script to update order
    Tested curl script to update order
    FEATURE WORKING
    Next stage will be to get-orders

--- scripts/orders/update-order.sh
+++ scripts/orders/update-order.sh
@@ -5,12 +5,14 @@ URL_PATH="/orders"
 curl "${API}${URL_PATH}/${ID}" \
   --include \
   --request PATCH \
-  --header "Authorization: Token token=${TOKEN}" \
   --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}" \
   --data '{
     "order": {
-      "old": "'"${OLDPW}"'",
-      "new": "'"${NEWPW}"'"
+      "shipping_address": "'"$SHIP"'",
+      "paid": "true",
+      "total": "0",
+      "billing_address": "'"$BILL"'"
     }
   }'
 
