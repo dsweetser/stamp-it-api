5e122070825e6b60a738f33b57db67acf19c1f6e
dsweetser <daniel.sweetser@gmail.com>
2017-03-29 15:54:02 -0400

    Refactored list_item serialized
    Side loaded just the id's for orders and stamps from list item for ease
      of use with Ember

--- app/serializers/list_item_serializer.rb
+++ app/serializers/list_item_serializer.rb
@@ -2,4 +2,12 @@ class ListItemSerializer < ActiveModel::Serializer
   attributes :id
   has_one :stamp
   has_one :order
+
+  def order
+    object.order.id
+  end
+
+  def stamp
+    object.stamp.id
+  end
 end
